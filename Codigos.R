34d099988950cf488f778c0c12dd02725cfaeffa
TwitchViejoSarmi <alesansarive@gmail.com>
2024-03-16 21:35:01 -0500

    MOD: Ajustado los parametros para que tenga mas sentido la evaluacion.

--- Codigos.R
+++ Codigos.R
@@ -5,12 +5,7 @@ experimento <- rbinom(5, size = 3, prob = 0.1)
 print(experimento)
 
 # Punto 1.
-muestras <- replicate(100, rbinom(1000, size = 3, prob = 0.1))
-f_n <- c() # Frecuencia Relativa.
-
-for (i in 1:100){
-  f_n <- c(f_n,sum(muestras[,i] == 3)/1000)
-}
+f_n <- replicate(100, sum((rbinom(10000, size = 3, prob = 0.1) == 3)/ 10000))
 
 # a). y b).
 plot(1:100,
@@ -19,17 +14,17 @@ plot(1:100,
      ylab="fi",
      xlab ="Simulación",
      sub="Figura 1. Gráfico de dispersión del fallo de 3 componentes después de 5 años")
-
-mid <- (max(f_n) + min(f_n)) / 2
 lines(1:100,
-      rep(0.29,100), col = "red")
+      rep(0.001,100), col = "red")
 
 # c).
-n <- c(2, 5, 10, 20, 30, 40, 50, 80, 150, 200, 400, 500, 600, 700, 800, 900, 1000, 2000, 5000, 10000)
+n <- c(20, 30, 40, 50, 100, 200, 300, 400, 1000, 20000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 200000, 500000, 1000000)
+length(n)
 f_n <- c() # Frecuencia Relativa.
 for (i in n){
-  f_n <- c(f_n,sum(rbinom(i, size=3,prob=0.1) == 3)/i)
+  f_n <- c(f_n,round(sum(rbinom(i, size=3,prob=0.1) == 3)/i, 3))
 }
+print(f_n)
 plot(1:20,
      f_n,
      pch=19,
@@ -37,3 +32,5 @@ plot(1:20,
      ylab="fi (%)",
      xlab ="n",
      sub="Figura 2. Diagrama de dispersión de las frecuencias relativas segun el crecimiento de n")
+lines(1:20,
+      rep(0.001,20), col = "red")
