93ecc6329dc469647d7977a6c5a539b0040f2ac2
TwitchViejoSarmi <95054605+TwitchViejoSarmi@users.noreply.github.com>
2024-03-16 23:22:42 -0500

    NEW: Terminado de ajustar parametros de las funciones.

--- Codigos.R
+++ Codigos.R
@@ -1,13 +1,13 @@
 set.seed(101) # Semilla de aleatoriedad.
 
-# Se asume que 1 indica que el componente
+# Se asume que 3 indica que los 3 componentes fallaron.
 experimento <- rbinom(5, size = 3, prob = 0.1)
 print(experimento)
 
 # Punto 1.
 f_n <- replicate(100, sum((rbinom(10000, size = 3, prob = 0.1) == 3)/ 10000))
 
-# a). y b).
+# a), b) y c).
 plot(1:100,
      f_n,pch=19,
      main = "Dispersión de fallo para 3 componentes",
@@ -17,7 +17,7 @@ plot(1:100,
 lines(1:100,
       rep(0.001,100), col = "red")
 
-# c).
+# Punto 2.
 n <- c(20, 30, 40, 50, 100, 200, 300, 400, 1000, 20000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 200000, 500000, 1000000)
 length(n)
 f_n <- c() # Frecuencia Relativa.
